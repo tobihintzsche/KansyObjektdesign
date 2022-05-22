abd2f7a7524fa34337e4330c82ae740d0ecef53d
Tobias Hintzsche <tobihintzsche2000@gmail.com>
2022-05-22 15:16:28 +0200

    npm i ImageSlider

--- kansyobjektdesign/pages/index.tsx
+++ kansyobjektdesign/pages/index.tsx
@@ -1,14 +1,32 @@
 import Link from 'next/link'
 import Layout from '../components/Layout'
 
+import ImageGallery from 'react-image-gallery';
+
+import "react-image-gallery/styles/css/image-gallery.css";
+
+
+const images = [
+  {
+    original: 'https://picsum.photos/id/1018/1000/600/',
+    thumbnail: 'https://picsum.photos/id/1018/250/150/',
+  },
+  {
+    original: 'https://picsum.photos/id/1015/1000/600/',
+    thumbnail: 'https://picsum.photos/id/1015/250/150/',
+  },
+  {
+    original: 'https://picsum.photos/id/1019/1000/600/',
+    thumbnail: 'https://picsum.photos/id/1019/250/150/',
+  },
+];
+
+
+
+
 const IndexPage = () => (
   <Layout title="Home | Next.js + TypeScript Example">
-    <h1>Hello Next.js 👋</h1>
-    <p>
-      <Link href="/about">
-        <a>About</a>
-      </Link>
-    </p>
+    {/* ImageSlider: https://react-slideshow-image.netlify.app/ */}
   </Layout>
 )
 
